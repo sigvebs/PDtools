254a0479e3a867a0a92a8b2edcbce5cdb9572e86
Sigve Bøe Skattum <sigvebs@gmail.com>
2017-12-03 17:33:45 +0100

    Added new config.h to the project.

---
+++ src/PDtools/config.h
@@ -0,0 +1,29 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <string>
+#include <iostream>
+#include <armadillo>
+#include <map>
+#include <unordered_map>
+#include <vector>
+
+namespace PDtools
+{
+using std::cout;
+using std::endl;
+using std::cerr;
+using std::map;
+using std::unordered_map;
+using std::vector;
+using std::string;
+using std::string;
+using arma::mat;
+using arma::ivec;
+using arma::vec;
+using arma::vec2;
+using arma::vec3;
+using arma::zeros;
+}
+
+#endif // CONFIG_H
