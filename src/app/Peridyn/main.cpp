7ba7deec8e1e4b91717bba43f306b29b6134fa37
Sigve Bøe Skattum <sigvebs@gmail.com>
2017-12-03 16:49:00 +0100

    Updated to google style standards.

--- src/app/Peridyn/main.cpp
+++ src/app/Peridyn/main.cpp
@@ -2,34 +2,32 @@
 #include <mpi.h>
 #endif
 
-#include <iostream>
+#include <string>
 #include <armadillo>
 #include <memory>
 #include "pdsolver.h"
 
-int main(int argc, char** argv)
-{
-    using namespace arma;
-    using namespace std;
+int main(int argc, char** argv) {
+    using std::string;
+    using arma::wall_clock;
 
-
-    if(argc != 2) {
+    if (argc != 2) {
         cerr << "usage: peridyn [path to config file]" << endl;
         return 1;
     }
 
     wall_clock timer;
 #ifdef USE_MPI
-    MPI::Init (argc, argv);
-    const int myRank = MPI::COMM_WORLD.Get_rank( );
-    const int nCores = MPI::COMM_WORLD.Get_size( );
+    MPI::Init(argc, argv);
+    const int myRank = MPI::COMM_WORLD.Get_rank();
+    const int nCores = MPI::COMM_WORLD.Get_size();
 #else
     const int myRank = 0;
     const int nCores = 1;
 #endif
     string configPath = argv[1];
 
-    if(myRank == 0) {
+    if (myRank == 0) {
         cout << "Starting with " << nCores << endl;
         cout << "input file: " << configPath << endl;
     }
@@ -39,7 +37,7 @@ int main(int argc, char** argv)
     timer.tic();
     Solver.solve();
 
-    if(myRank == 0){
+    if (myRank == 0) {
         double nSec = timer.toc();
 
         int hours = nSec/3600;
@@ -54,7 +52,7 @@ int main(int argc, char** argv)
              << endl;
     }
 #ifdef USE_MPI
-    MPI::Finalize( );
+    MPI::Finalize();
 #endif
 
     return 0;
